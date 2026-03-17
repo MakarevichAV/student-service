c267ff021ca3d08286c632c92e450ace95985882
MakarevichAV <makarevich.a.v@yandex.ru>
2026-03-17 20:37:41 +0200

    bla bla bla

--- src/controller/studentController.js
+++ src/controller/studentController.js
@@ -1,7 +1,12 @@
-import * as repo from '../repository/studentRepository.js';
+import * as service from '../service/studentService.js';
+import {addStudentSchema, scoreSchema, updateStudentSchema} from "../validator/studentValidator.js";
 
 export const addStudent = async (req, res) => {
-    const success = await repo.addStudent(req.body);
+    const {error} = addStudentSchema.validate(req.body)
+    if (error) {
+        return res.status(400).send(error.details[0].message)
+    }
+    const success = await service.addStudent(req.body);
     if (success) {
         res.status(204).send();
     } else {
@@ -10,37 +15,45 @@ export const addStudent = async (req, res) => {
 }
 
 export const findStudent = async (req, res) => {
-    const student = await repo.findStudent(+req.params.id);
+    const student = await service.findStudent(+req.params.id);
     if (student) {
-        const {password, ...studentWithoutPassword} = student;
-        res.json(studentWithoutPassword);
+        // const {password, ...studentWithoutPassword} = student;
+        res.json(student);
     } else {
         res.status(404).send();
     }
 }
 
 export const deleteStudent = async (req, res) => {
-    const student = await repo.deleteStudent(+req.params.id);
+    const student = await service.deleteStudent(+req.params.id);
     if (student) {
-        const {password, ...studentWithoutPassword} = student;
-        res.json(studentWithoutPassword);
+        // const {password, ...studentWithoutPassword} = student;
+        res.json(student);
     } else {
         res.status(404).send();
     }
 }
 
 export const updateStudent = async (req, res) => {
-    const student = await repo.updateStudent(+req.params.id, req.body);
+    const {error} = updateStudentSchema.validate(req.body)
+    if (error) {
+        return res.status(400).send(error.details[0].message)
+    }
+    const student = await service.updateStudent(+req.params.id, req.body);
     if (student) {
-        const {scores, ...studentWithoutScore} = student;
-        res.json(studentWithoutScore);
+        // const {scores, ...studentWithoutScore} = student;
+        res.json(student);
     } else {
         res.status(404).send();
     }
 }
 
 export const addScore = async (req, res) => {
-    const success = await repo.addScore(+req.params.id, req.body.examName, req.body.score);
+    const {error} = scoreSchema.validate(req.body)
+    if (error) {
+        return res.status(400).send(error.details[0].message)
+    }
+    const success = await service.addScore(+req.params.id, req.body.examName, req.body.score);
     if (success) {
         res.status(204).send();
     } else {
@@ -55,22 +68,22 @@ export const addScore = async (req, res) => {
 }
 
 export const findStudentsByName = async (req, res) => {
-    const data = await repo.findByName(req.params.name);
-    const students = data.map(({password, ...studentWithoutPassword}) => studentWithoutPassword);
+    const students = await service.findByName(req.params.name);
+    // const students = data.map(({password, ...studentWithoutPassword}) => studentWithoutPassword);
     res.json(students);
 }
 
 export const studentsCountByNames = async (req, res) => {
-    let count = 0;
-    const names = req.query.names;
-    const list = Array.isArray(names) ? names : [names];
-    if (names) {
-        count = await repo.countByNames(list)
-    }
+
+    const names = Array.isArray(req.query.names) ? req.query.names : [req.query.names];
+    // const list = Array.isArray(names) ? names : [names];
+
+    const count = await service.countByNames(names)
+
     res.status(200).send(count);
 }
 
 export const findStudentByMinScore = async (req, res) => {
-    const students = await repo.findByMinScore(req.params.exam, +req.params.minScore);
+    const students = await service.findByMinScore(req.params.exam, +req.params.minScore);
     res.json(students);
 }
--- src/repository/studentRepository.js
+++ src/repository/studentRepository.js
@@ -1,76 +1,113 @@
-let collection;
+import Student from "../model/student.js";
 
-export const init = db => collection = db.collection("college");
-
-export const addStudent = async ({id, name, password}) => {
-    const existingStudent = await collection.findOne({_id: id});
-    if (existingStudent) {
-        return false
-    }
-    await collection.insertOne({_id: id, name, password, scores: {}});
-    return true;
+export function createStudent(student) {
+    return Student.create(student)
 }
 
-export const findStudent = async (id) => {
-    const student = await collection.findOne({_id: id});
-    return renameId(student);
+export function findStudentById(id) {
+    return Student.findById(id)
 }
 
-export const deleteStudent = async (id) => {
-    const student = await collection.findOneAndDelete({_id: id});
-    return renameId(student);
+export function deleteStudentById(id) {
+    return Student.findByIdAndDelete(id)
 }
 
-export const updateStudent = async (id, data) => {
-    const student = await collection.findOneAndUpdate(
-        {_id: id},
-        {$set: data},
-        {returnDocument: 'after'}
-    );
-    return renameId(student);
+export function updateStudent(id, data) {
+    return Student.findByIdAndUpdate(id, data)
 }
 
-export const addScore = async (id, exam, score) => {
-    const student = await collection.findOneAndUpdate(
-        {_id: id},
-        {$set: {[`scores.${exam}`]: score}},
-        {returnDocument: 'after'}
-    );
-    return !!student;
+export function updateScore(id, exam, score) {
+    return Student.findByIdAndUpdate(id, {$set: {[`scores.${exam}`]: score}})
 }
 
-export const findByName = async (name) => {
-    // const data = await collection.find({name: new RegExp(`^${name}$`, "i")}).toArray();
-    const students = [];
-    const cursor = await collection.find({name: new RegExp(`^${name}$`, "i")});
-    while(await cursor.hasNext()) {
-        students.push(renameId(await cursor.next()));
-    }
-    return students;
+export function findStudentsByName(name) {
+    return Student.find({name: new RegExp(`^${name}$`,  'i')})
 }
 
-export const countByNames = async (names) => {
-    let count = 0;
-    for (const name of names) {
-        count += (await collection.find({name: new RegExp(`^${name}$`, "i")}).toArray()).length
-    }
-    return count;
+export function countStudentsByName(name) {
+    const regexCondition = names.map(name => ({
+        name: new RegExp(`^${name}$`,  'i')
+    }));
+    return Student.countDocuments({$or: regexCondition})
 }
 
-export const findByMinScore = async (exam, minScore) => {
-    // return (await collection.find({[`scores.${exam}`]: {$gte: minScore}}).toArray()).map(renameId);
-    const students = [];
-    const cursor = await collection.find({[`scores.${exam}`]: {$gte: minScore}});
-    for await (const student of cursor) {
-        students.push(renameId(student));
-    }
-    return students;
+export function findStudentsMinScore(exam, minScore) {
+    return Student.find({[`scores.${exam}`]: {$gte: minScore}})
 }
 
-function renameId(student) {
-    if (student) {
-        student.id = student._id;
-        delete student._id;
-    }
-    return student;
-}
+// let collection;
+//
+// export const init = db => collection = db.collection("college");
+//
+// export const addStudent = async ({id, name, password}) => {
+//     const existingStudent = await collection.findOne({_id: id});
+//     if (existingStudent) {
+//         return false
+//     }
+//     await collection.insertOne({_id: id, name, password, scores: {}});
+//     return true;
+// }
+//
+// export const findStudent = async (id) => {
+//     const student = await collection.findOne({_id: id});
+//     return renameId(student);
+// }
+//
+// export const deleteStudent = async (id) => {
+//     const student = await collection.findOneAndDelete({_id: id});
+//     return renameId(student);
+// }
+//
+// export const updateStudent = async (id, data) => {
+//     const student = await collection.findOneAndUpdate(
+//         {_id: id},
+//         {$set: data},
+//         {returnDocument: 'after'}
+//     );
+//     return renameId(student);
+// }
+//
+// export const addScore = async (id, exam, score) => {
+//     const student = await collection.findOneAndUpdate(
+//         {_id: id},
+//         {$set: {[`scores.${exam}`]: score}},
+//         {returnDocument: 'after'}
+//     );
+//     return !!student;
+// }
+//
+// export const findByName = async (name) => {
+//     // const data = await collection.find({name: new RegExp(`^${name}$`, "i")}).toArray();
+//     const students = [];
+//     const cursor = await collection.find({name: new RegExp(`^${name}$`, "i")});
+//     while(await cursor.hasNext()) {
+//         students.push(renameId(await cursor.next()));
+//     }
+//     return students;
+// }
+//
+// export const countByNames = async (names) => {
+//     let count = 0;
+//     for (const name of names) {
+//         count += (await collection.find({name: new RegExp(`^${name}$`, "i")}).toArray()).length
+//     }
+//     return count;
+// }
+//
+// export const findByMinScore = async (exam, minScore) => {
+//     // return (await collection.find({[`scores.${exam}`]: {$gte: minScore}}).toArray()).map(renameId);
+//     const students = [];
+//     const cursor = await collection.find({[`scores.${exam}`]: {$gte: minScore}});
+//     for await (const student of cursor) {
+//         students.push(renameId(student));
+//     }
+//     return students;
+// }
+//
+// function renameId(student) {
+//     if (student) {
+//         student.id = student._id;
+//         delete student._id;
+//     }
+//     return student;
+// }
